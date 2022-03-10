a794d51feaccbf61f90f0b6b54723333445a8d51
Karen Ngugi <31765863+KarenNgugi@users.noreply.github.com>
2022-03-10 14:49:22 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fputs(s, stderr);
 	return (1);
 }
