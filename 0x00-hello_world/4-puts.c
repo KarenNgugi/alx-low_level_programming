d74866193364f1fd12f99c9b09d33d644677a3a1
KarenNgugi <karenwngugi12@gmail.com>
2022-03-10 13:00:52 +0300

    added betty doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry Point
+ *
+ * Return: returns 0 if code executes successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
