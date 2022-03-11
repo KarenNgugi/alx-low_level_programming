4d68264391bdeef439843764712c83880f347e20
Karen Ngugi <31765863+KarenNgugi@users.noreply.github.com>
2022-03-11 13:11:09 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
 /**
  * main - entry point
@@ -12,7 +11,7 @@ int main(void)
 	char abc[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 25; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(abc[i]);
 	}
