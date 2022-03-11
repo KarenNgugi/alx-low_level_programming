bb6b124f0f73527ce94e4a07528c79c1993aaaa7
KarenNgugi <karenwngugi12@gmail.com>
2022-03-11 15:56:32 +0300

    advanced task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,7 @@ int main(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
-	{
 		for (j = i + 1; j < 10; j++)
-		{
 			for (k = j + 1; k < 10; k++)
 			{
 				if (i != j && j != k && k != i)
@@ -30,8 +28,6 @@ int main(void)
 					putchar(' ');
 				}
 			}
-		}
-	}
 	putchar('\n');
 	return (0);
 }
