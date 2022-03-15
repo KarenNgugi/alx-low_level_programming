73b44f025c72ee5d3a5ebdb3a22bbc5e0294ebec
KarenNgugi <karenwngugi12@gmail.com>
2022-03-15 13:01:10 +0300

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,29 +2,6 @@
 #include <ctype.h>
 #include "main.h"
 
-int _islower(int c);
-
-/**
- * main - program entry point
- *
- * Return: 0 if successful
- */
-
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
 /**
  * _islower - checks if value is lowercase
  * @c: value to be checked
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,11 +3,8 @@
 
 int _putchar(int c);
 
-#endif
-
-#ifndef PRINT_ALPHABET
-#define PRINT_ALPHABET
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+
 #endif
