4adbb81a085c2e5eae128122d340ffa8c3501449
KarenNgugi <karenwngugi12@gmail.com>
2022-03-15 13:12:07 +0300

    tasks 4 & 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,30 +1,6 @@
 #include <ctype.h>
 #include "main.h"
 
-int _isalpha(int c);
-
-/**
- * main - program entry point
- *
- * Return: 0 if successful
- */
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
 /**
  * _isalpha - checks if value is alphabetic
  * @c:  input value
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,40 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-int print_sign(int n);
-
-/**
- * main - program starting point
- *
- * Return: 0 if successful
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
 /**
  * print_sign - prints the sign of a number and returns the number
  * @n: number to check its sign
