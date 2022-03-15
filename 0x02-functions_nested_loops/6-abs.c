afa3af12e693e28a46477b97f830c7b02a8514f9
KarenNgugi <karenwngugi12@gmail.com>
2022-03-15 13:23:55 +0300

    tasks 6 & 7

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 
-int _abs(int);
-
-/**
- * main - program entry point
- *
- * Return: 0 if successful
- */
-int main(void)
-{
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
-}
-
 /**
  * _abs - returns absolute value of the number input
  * @int: number to be absoluted
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 
-int print_last_digit(int);
-
-/**
- * main - entry point
- *
- * Return: 0 if successful
- */
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
-}
-
 /**
  * print_last_digit - prints & returns the last digit of a number
  * @int: number to be checked
