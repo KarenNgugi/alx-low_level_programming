96c2c8773e920850f0656116a2533abf72b7c702
KarenNgugi <karenwngugi12@gmail.com>
2022-03-16 14:04:34 +0300

    probably fixed it

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - main
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d\n", i);
-	}
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
