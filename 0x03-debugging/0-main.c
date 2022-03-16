60c77e364997ebb9c046e657f43629d35fd173ff
KarenNgugi <karenwngugi12@gmail.com>
2022-03-16 10:44:37 +0300

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check if number is positive, negative, or zero
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("0 is zero\n");
+	return (0);
+}
