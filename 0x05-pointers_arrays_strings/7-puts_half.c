f12125b1420705198e343938061a3885c13b95f2
KarenNgugi <karenwngugi12@gmail.com>
2022-03-21 11:19:44 +0300

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,16 @@ void puts_half(char *str)
 	unsigned int len, i;
 
 	if (strlen(str) % 2 == 0)
+	{
 		len = strlen(str) / 2;
+		for (i = len; i < strlen(str); i++)
+                	_putchar(str[i]);
+	}
 	else
+	{
 		len = (strlen(str) - 1) / 2;
-
-	for (i = len; i < strlen(str); i++)
-		_putchar(str[i]);
+		for (i = len; i < strlen(str); i++)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
