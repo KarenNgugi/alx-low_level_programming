f498dfcbefe819b32be3505691563f4233a54222
Karen Ngugi <31765863+KarenNgugi@users.noreply.github.com>
2022-03-24 09:42:56 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,25 +2,6 @@
 
 
 /**
- * putchar_many - prints out number using putchar
- * @n: integer to be printed
- */
-
-void putchar_many(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-		putchar_many(n / 10);
-
-	_putchar(n % 10 + '0');
-}
-
-/**
  * reverse_array - print array in reverse
  * @a: array of integers to reverse
  * @n: number of elements in array
@@ -28,18 +9,11 @@ void putchar_many(int n)
 
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, arr[n];
 
-	for (i = n - 1; i >= 0; i--)
-	{
-		if (i != 0)
-		{
-			putchar_many(a[i]);
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-			putchar_many(a[i]);
-	}
-	_putchar('\n');
+	for (i = 0; i < n; i++)
+		arr[n - 1 - i] = a[i];
+	
+	for (i = 0; i < n; i++)
+		a[i] = arr[i];
 }
