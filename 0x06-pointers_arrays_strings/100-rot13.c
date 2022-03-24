16d2765362d6bb522f05f8631633b3f1214f750b
KarenNgugi <karenwngugi12@gmail.com>
2022-03-24 15:09:39 +0300

    fixed task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * rot13 - rotates the letters in a string by 13
@@ -10,15 +11,22 @@ char *rot13(char *c)
 {
 	char l1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char l2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *d = malloc(strlen(c) * sizeof(char));
 	unsigned int i, j;
+
+	strcpy(d, c);
 
 	for (i = 0; i < strlen(c); i++)
 	{
 		for (j = 0; j < strlen(l1); j++)
 		{
 			if (c[i] == l1[j])
-				c[i] = l2[j];
+				d[i] = l2[j];
 		}
 	}
+
+	for (i = 0; i < strlen(c); i++)
+		c[i] = d[i];
+
 	return (c);
 }
