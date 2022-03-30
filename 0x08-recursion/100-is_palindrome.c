99d75f349156215badd768c8e79604f9a068801c
KarenNgugi <karenwngugi12@gmail.com>
2022-03-30 15:26:04 +0300

    task 6

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if string is a palindrome or not
+ * @s: string to check
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int flag;
+
+	if (s == '')
+		flag = 0;
+
+	else
+	{
+	}
+
+	return (flag);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <unistd.h>
 
 int _putchar(int c);
