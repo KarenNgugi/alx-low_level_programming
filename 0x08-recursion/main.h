117dda085c7e45387e304f0d250b1e0322a98302
KarenNgugi <karenwngugi12@gmail.com>
2022-03-31 11:39:43 +0300

    fixed task 5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,26 +18,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-
-#define TOL (0.001)     /* Error tolerance bound */
-#define FABS(a)   ((a)>0.0?(a):-(a))
-double sq_root(double x);
-/**
- * sq_root - calculate squareroot
- * @x: number to calculate squareroot
- * Return: double
- */
-double sq_root(double x)
-{
-        double rt = 1, ort = 0, diff = 0;
-
-        while (FABS(ort - rt) > TOL)
-        {
-                ort = rt;
-                rt = ((x / rt) + rt) / 2;
-                diff = ort - rt;
-        }
-        return (rt);
-}
-
 #endif /* MAIN_H */
