333d67c4fadc6ead8ee37f80ed00a4291c3d8f5b
KarenNgugi <karenwngugi12@gmail.com>
2022-04-05 11:05:33 +0300

    fixed 'mixed declarations forbidden' error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,16 +8,19 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, len = strlen(s1) + strlen(s2) + 1;
-        char *s = malloc(len * sizeof(char));
+	unsigned int i;
+	char *s;
+	
+	if (s1 == NULL)
+                s1 = "";
+
+        if (s2 == NULL)
+                s2 = "";
+
+	s = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+
 	if (s == NULL)
                 return (NULL);
-
-	if (s1 == NULL)
-		s1 = "";
-
-	else if (s2 == NULL)
-		s2 = "";
 
 	for (i = 0; i < strlen(s1); i++)
 		s[i] = s1[i];
@@ -25,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < strlen(s2); i++)
 		s[strlen(s1) + i] = s2[i];
 
-	s[len] = '\0';
+	s[strlen(s1) + strlen(s2) + 1] = '\0';
 
 	return (s);
 	free(s);
