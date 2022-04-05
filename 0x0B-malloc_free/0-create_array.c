2799c7ab47b779c261276c85ff8847b864635673
KarenNgugi <karenwngugi12@gmail.com>
2022-04-05 10:23:36 +0300

    handled malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,9 @@ char *create_array(unsigned int size, char c)
 		char *s = (char *)malloc(size * sizeof(char));
 		unsigned int i;
 
+		if (s == NULL)
+			return (NULL);
+
 		for (i = 0; i < size; i++)
 			s[i] = c;
 
