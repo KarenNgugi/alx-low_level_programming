6bea58c651688602e544c7125a4938cada889646
KarenNgugi <karenwngugi12@gmail.com>
2022-04-11 13:49:13 +0300

    update task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *name_copy, *owner_copy;
 	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
@@ -17,6 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
+
+	name_copy = name;
+	owner_copy = owner;
 
 	return (dog);
 }
