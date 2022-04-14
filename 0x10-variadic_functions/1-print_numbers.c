2f30642610bd56774d9d95fe4bae4f6cff696d47
KarenNgugi <karenwngugi12@gmail.com>
2022-04-14 12:33:09 +0300

    fixed task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,10 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator != NULL || n != 0)
+	if (n != 0)
 	{
-
 		va_start(args, n);
+
+		if (separator == NULL)
+			separator = "";
 
 		for (i = 0; i < n; i++)
 		{
