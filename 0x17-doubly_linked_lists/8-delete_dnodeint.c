12a7e6b6e4e36451f0a3dba9993f7da8e65e9116
KarenNgugi <karenwngugi12@gmail.com>
2022-07-09 23:07:22 +0300

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns length of linked list
+ * @h: doubly linked list
+ * Return: number of elements present
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes node at given index
+ * @head: head node of specific list
+ * @index: position to remove node at
+ * Return: 1 if successful, otherwise -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	int result = -1;
+	dlistint_t *h = *head, *p;
+	unsigned int count = 0;
+
+	if (*head != NULL)
+	{
+		if (index == 0)
+		{
+			if (h->next)
+			{
+				h = h->next;
+				h->prev = NULL;
+				*head = h;
+				result = 1;
+			}
+			else
+			{
+				*head = NULL;
+				result = 1;
+			}
+		}
+
+		else if (index < dlistint_len(*head))
+		{
+			for (count = 0; count < index; count++)
+			{
+				p = h;
+				h = h->next;
+			}
+			p->next = h->next;
+			h->next->prev = p;
+			result = 1;
+		}
+	}
+	return (result);
+}
